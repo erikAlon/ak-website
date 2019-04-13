68a334bb38ac96b44da0d91ba7c0a1d14e3db9f1
Dante <mr.alonsoea@protonmail.ch>
2019-04-12 20:22:59 -0500

    implemented gatsby-image-background on layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,12 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 import "./layout2.css"
+import BackgroundImage from "gatsby-background-image"
 
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
+      query {
         site {
           siteMetadata {
             title
@@ -17,19 +18,32 @@ const Layout = ({ children }) => (
             client
           }
         }
+        desktop: file(relativePath: { eq: "AK.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 3000, maxHeight: 3001) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     `}
     render={data => (
       <div className="layout">
-        <div className="layout--overlay" />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div className="layout__main">
-          <main>{children}</main>
-          <footer className="layout__foot">
-            © {new Date().getFullYear()}, {data.site.siteMetadata.client} |{" "}
-            {data.site.siteMetadata.author}
-          </footer>
-        </div>
+        <BackgroundImage
+          fluid={data.desktop.childImageSharp.fluid}
+          alt="ak logo"
+          className="bgImage"
+        >
+          <Header siteTitle={data.site.siteMetadata.title} />
+
+          <div className="layout__main">
+            <main>{children}</main>
+            <footer className="layout__foot">
+              © {new Date().getFullYear()}, {data.site.siteMetadata.client} |{" "}
+              {data.site.siteMetadata.author}
+            </footer>
+          </div>
+        </BackgroundImage>
       </div>
     )}
   />
