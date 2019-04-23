a834f2c4db3686db5646bec7b5628ba4828ea6ba
Dante <mr.alonsoea@protonmail.ch>
2019-04-22 23:28:17 -0500

    set col and row and removed jumbotron from contact features

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,6 @@ import { InquiryForm } from "../components/Form"
 
 import { Jumbotron, Container, Row, Col } from "react-bootstrap"
 
-import Img from "gatsby-image"
-
 export default ({ data }) => (
   <Layout>
     <Container className="contact__main">
@@ -28,44 +26,59 @@ export default ({ data }) => (
         </Col>
       </Row>
 
-      <Jumbotron>
-        <Container className="contact__main__features">
-          <Row>
-            <Col>
-              <i class="fas fa-compact-disc" />A unique sound born from the
-              southern part of Texas
-            </Col>
-            <Col>
-              <i class="fas fa-calendar-alt" />
+      <Container className="contact__main__features">
+        <Row>
+          <Col>
+            <i class="fas fa-compact-disc" />
+          </Col>
+          <Col xs={4}>
+            <h3>Expertise</h3>
+            <span>A unique sound born from the southern part of Texas</span>
+          </Col>
+          <Col>
+            <i class="fas fa-calendar-alt" />
+          </Col>
+          <Col xs={4}>
+            <h3>Fast turn-around</h3>
+            <span>
               Get your tracks mixed and mastered within 3 business days
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Container className="circle__container">
-                <Row>
-                  <i id="deg0" class="fab fa-itunes-note" />
-                  <i id="deg45" class="fab fa-youtube" />
-                </Row>
-                <Row>
-                  <i id="deg135" class="fab fa-google-play" />
-                  <i id="deg180" class="fab fa-soundcloud" />
-                </Row>
-                <Row>
-                  <i id="deg225" class="fab fa-spotify" />
-                  <i id="deg315" class="fab fa-bandcamp" />
-                </Row>
-              </Container>
+            </span>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <Container className="circle__container">
+              <Row>
+                <i id="deg0" class="fab fa-itunes-note" />
+                <i id="deg45" class="fab fa-youtube" />
+              </Row>
+              <Row>
+                <i id="deg135" class="fab fa-google-play" />
+                <i id="deg180" class="fab fa-soundcloud" />
+              </Row>
+              <Row>
+                <i id="deg225" class="fab fa-spotify" />
+                <i id="deg315" class="fab fa-bandcamp" />
+              </Row>
+            </Container>
+          </Col>
+          <Col xs={4}>
+            <h3>Stream worthy</h3>
+            <span>
               Receive a stream-ready mastered file and an uncompressed mix-level
               file for archiving and future remastering
-            </Col>
-            <Col>
-              <i class="fas fa-layer-group" />
-              Four free revisions and capability of adding more
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
+            </span>
+          </Col>
+          <Col>
+            <i class="fas fa-layer-group" />
+          </Col>
+          <Col xs={4}>
+            <h3>4 Revisions</h3>
+            <span>Four free revisions and capability of adding more</span>
+          </Col>
+        </Row>
+      </Container>
 
       <Row className="contact__main__services">
         <Col>&lt;services as cards&gt;</Col>
@@ -76,15 +89,3 @@ export default ({ data }) => (
     </Container>
   </Layout>
 )
-
-export const query = graphql`
-  query {
-    icons: file(relativePath: { eq: "mediafire.png" }) {
-      childImageSharp {
-        fixed(width: 40, height: 40) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`
