3d8055ba76eca82c34d0705ac4dc479ae29ab827
Dante <mr.alonsoea@protonmail.ch>
2019-04-27 00:17:37 -0500

    added sidenav to iphone portrait

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,42 @@ import React from "react"
 import { Breadcrumb, Container, Row, Col } from "react-bootstrap"
 import "../styling/header.css"
 
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "190px"
+}
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0"
+}
+
+const Sidenav = () => (
+  <div id="mySidenav" className="sidenav">
+    <i className="closebtn" onClick={closeNav} class="fas fa-times" />
+    <Link className="header__link" to="/">
+      About
+    </Link>
+    <Link className="header__link" to="store">
+      Merch
+    </Link>
+    <Link className="header__link" to="discography">
+      Samples
+    </Link>
+    <Link className="header__link" to="contact">
+      Upload
+    </Link>
+  </div>
+)
+
 const Header = ({ siteTitle }) => (
   <header>
+    <Sidenav />
     <Container className="header__main">
       <Row>
-        <Col>
+        <Col xs={9}>
           <h1>{siteTitle}</h1>
+        </Col>
+        <Col>
+          <i onClick={openNav} class="fas fa-bars" />
         </Col>
       </Row>
 
